89e9a71d6e808587999dc9f986c7369a7872602c
alfonsora6 <megaalfonso84@gmail.com>
2023-01-23 18:38:55 +0000

    cambios

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9f0h)gozf$g%6igo8&767w1xro0adm+)msxe)!eic$!fhvynb8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.22.201.43','python.alfonso.gonzalonazareno.org']
 
 
 # Application definition
@@ -121,3 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+
+STATIC_ROOT = "/var/www/html/django_tutorial/static/"
